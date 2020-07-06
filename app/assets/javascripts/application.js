6553f9f32e33a199f82f3ddf6511f50abad98799
Christophe Deschamps <christophe.d66@gmail.com>
2020-07-06 12:39:33 -0400

    Included jQuery and SemanticUI, added navigation HTML to index page.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+// Loads all Semantic javascripts
+
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require semantic-ui
+//= require tree .
+
